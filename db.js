f2892b7f024f6d6904518400a0e9fbe46ccb9eff
ericpkatz <ericpkatz@gmail.com>
2022-08-25 11:02:55 -0400

    commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const { TEXT, STRING } = Sequelize;
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_images_db');
+
+const Image = conn.define('image', {
+  name: {
+    type: STRING
+  },
+  data: {
+    type: TEXT
+  }
+});
+
+Image.addHook('beforeSave', (image)=> {
+  if(!image.data.startsWith('data')){
+    image.data = `data:image/png;base64, ${image.data}`;
+  }
+});
+
+module.exports = {
+  conn,
+  Image
+};
